734172006c38f54509b069e3b0c46cb4877a4f62
Juli Arnalot <juli.arnalot@xing.com>
2021-11-25 15:59:10 +0100

    code restructured

--- macro-demo/src/lib.rs
+++ macro-demo/src/lib.rs
@@ -4,12 +4,25 @@ use quote::quote;
 use syn::*;
 
 #[proc_macro_attribute]
-pub fn test(_metadata: TokenStream, input: TokenStream) -> TokenStream {
-    // parsing rust function to easy to use struct
-    // let input_fn = parse_macro_input!(input as ItemFn);
-    println!("input: {}", _metadata);
-    println!("input: {}", input);
-    TokenStream::from(quote! {fn dummy(){}})
+pub fn test_custom(_metadata: TokenStream, input: TokenStream) -> TokenStream {
+    let input_fn = parse_macro_input!(input as ItemFn);
+    let block = &input_fn.block;
+    let name = input_fn.sig.ident;
+
+    let code = quote! {
+        register_test! {
+            testframework::Test {
+                name: std::stringify!(#name),
+                file: std::file!(),
+                line: std::line!(),
+                handler: std::boxed::Box::new(|| {
+                    #block
+                }),
+            }
+        }
+    };
+
+    TokenStream::from(code)
 }
 
 mod tests {
--- test-framework/src/main.rs
+++ test-framework/src/main.rs
@@ -1,23 +1,23 @@
-// use std::env;
-// use testframework::Config;
-// use std::process;
+use std::env;
+use file_search::Config;
+use std::process;
 
 fn main() {
     // do_something()
-    // let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    // let config = Config::new(&args).unwrap();
+    let config = Config::new(&args).unwrap();
 
     // // let config = Config::new(&args).unwrap_or_else(|err| {
     // //     println!("Problem parsing arguments: {}", err);
     // //     process::exit(1);
     // // });
 
-    // println!("Searching for {}", config.query);
-    // println!("In file {}", config.filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    // if let Err(e) = testframework::run(config) {
-    //     println!("Application error: {}", e);
-    //     process::exit(1);
-    // }
+    if let Err(e) = file_search::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
